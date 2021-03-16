8d5548be693caa5db302b17179145252903bd708
Tramchandani <tramchandani@gammastack.com>
2021-03-16 11:33:27 +0530

    changes in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'sessions#index'
+  root to: 'home#index'
   get 'sessions/logout', to: 'sessions#logout', as: 'logout'
   get 'sessions/login', to: 'sessions#login', as: 'login'
   post 'sessions/authenticate', to: 'sessions#authenticate', as: 'authenticate'
