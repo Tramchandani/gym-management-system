549a1b6d3a1412b92b755b808d76130b1316bbc5
Tramchandani <79201195+Tramchandani@users.noreply.github.com>
2021-03-17 08:29:10 +0530

    bootstrap integration (#2)
    * authentication flow: sigup, login and logout functionality added
    
    * authentication flow: sigup, login and logout functionality added
    
    * added seperate session controller for login and logout functionality and made other changes requested in PR
    
    * shifted index page to session controller and minor fixes
    
    * bootstrap integration
    
    * added partial for displays notice and fixed indentation and spacing
    
    * some changes in the helper method definition
    
    * changes in routes
    
    * undo routes changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_secure_password
   validates :first_name, :email, :password, presence: true
-  validates :password, length: { in: 3..20 },  confirmation: true
+  validates :password, length: { in: 3..20 }, confirmation: true
   validates :email, uniqueness: { case_sensitive: false }, format: { with: REGEX }
 end
