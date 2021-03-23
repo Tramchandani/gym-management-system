5a5a64829406165bd85dadcc7c07b14630d3765c
Tramchandani <79201195+Tramchandani@users.noreply.github.com>
2021-03-23 16:14:21 +0530

    User profile (#4)
    * authentication flow: sigup, login and logout functionality added
    
    * authentication flow: sigup, login and logout functionality added
    
    * added seperate session controller for login and logout functionality and made other changes requested in PR
    
    * shifted index page to session controller and minor fixes
    
    * bootstrap integration
    
    * added partial for displays notice and fixed indentation and spacing
    
    * some changes in the helper method definition
    
    * changes in routes
    
    * undo routes changes
    
    * view user prifile, update itand update password functionality
    
    * PR changes for user profile
    
    * PR chnages except removing bootstrap URL
    
    * fixed indentation
    
    * UI change to use bootstrap 4
    
    * added proper spaces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,68 @@
 class UsersController < ApplicationController
+  before_action :current_user, except: %i[new create]
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(signup_params) 
+    @user = User.new(user_params) 
     if @user.save 
       session[:user_id] = @user.id
-      redirect_to user_path(@user.id), notice: "signup successful"
+      flash[:notice] = "signup successful"
+      redirect_to user_path(@user.id)
     else
       render :new
     end
   end
 
   def show
-    @user = User.find_by_id(session[:user_id])   
+    render layout: 'empty'
+  end
+
+  def edit;  end
+
+  def update
+    if @user.update(user_update_params)
+      flash[:notice] = "profile updated"
+      redirect_to user_path(@user.id)
+    else
+      flash[:notice] = "profile updation failed"
+      render :edit
+    end
+  end
+
+  def edit_password;  end
+
+  def update_password
+    if @user&.authenticate(password_params[:current_password]) && confirm_update
+      flash[:notice] = "password updated"
+      redirect_to user_path(@user.id)
+    else
+      flash[:notice] = "password updation failed"
+      render :edit_password
+    end
   end
 
   private
 
-  def signup_params
+  def user_params
     params.require(:user).permit(:first_name, :last_name, :dob, :email, :password, :password_confirmation)
+  end
+
+  def user_update_params
+    params.require(:user).permit(:first_name, :last_name, :dob, :email)
+  end
+
+  def password_params
+    params.require(:user_pass).permit(:password, :password_confirmation, :current_password)
+  end
+
+  def current_user
+    @user ||= User.find_by_id(session[:user_id])
+  end
+
+  def confirm_update
+    @user.update(password: password_params[:password], password_confirmation: password_params[:password_confirmation])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_secure_password
-  validates :first_name, :email, :password, presence: true
-  validates :password, length: { in: 3..20 }, confirmation: true
+  validates :first_name, :email, presence: true
+  validates :password, presence: true, length: { in: 3..20 }, confirmation: true, if: :should_validate_password?
   validates :email, uniqueness: { case_sensitive: false }, format: { with: REGEX }
+
+  private
+
+  def should_validate_password?
+    new_record? || password.present?
+  end
 end
