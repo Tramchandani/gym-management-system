15a1753b5500abab250c8bdaf586718fd303e443
Tramchandani <79201195+Tramchandani@users.noreply.github.com>
2021-03-26 17:52:43 +0530

    Add Addresses (#6)
    * authentication flow: sigup, login and logout functionality added
    
    * authentication flow: sigup, login and logout functionality added
    
    * added seperate session controller for login and logout functionality and made other changes requested in PR
    
    * shifted index page to session controller and minor fixes
    
    * bootstrap integration
    
    * added partial for displays notice and fixed indentation and spacing
    
    * some changes in the helper method definition
    
    * changes in routes
    
    * undo routes changes
    
    * view user prifile, update itand update password functionality
    
    * active storage integration
    
    * use of strong params for image deletion
    
    * added multiple addresses functionality
    
    * PR changes and UI changes: upgrade to bootstrap 4
    
    * UI changes
    
    * fixed spaces and new lines
    
    * fixed indentation in js file
    
    * refactored code for displaying addresses and moved address form partial to shared folder
    
    * added show notice partial to layout file
    
    * removed an extra line
    
    * handled invalid address by raising an exception
    
    * added error page to show invalid address
    
    * removed action error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_secure_password
-  has_many_attached :images
+  has_many :addresses, dependent: :destroy
+  has_many_attached :images, dependent: :destroy
   validates :first_name, :email, presence: true
   validates :password, presence: true, length: { in: 3..20 }, confirmation: true, if: :should_validate_password?
   validates :email, uniqueness: { case_sensitive: false }, format: { with: REGEX }
